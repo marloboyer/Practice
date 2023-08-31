ddf923c13d34d1a4af9fab451126157a53c8b877
marloboyer <marlo.boyer7@yahoo.com>
2023-08-30 21:07:13 -0500

    did 3 problems

--- more-practice.rb
+++ more-practice.rb
@@ -598,6 +598,21 @@ p one_string
 #  4. Start with an array of numbers and compute the the minumum number.
 #     For example, [5, 10, 8, 3, 9] becomes 3.
 
+array = [5, 10, 8, 3, 9]
+minumum_number = array[0]
+
+index = 0
+
+while index < array.length
+  array = array[index]
+  if minumum_number < array
+    minumum_number = array
+  end
+  index += 1
+end
+
+p minumum_number
+
 #  5. Start with an array of strings and compute the total length of all the strings.
 #     For example, ["volleyball", "basketball", "badminton"] becomes 29.
 
@@ -621,11 +636,36 @@ p product
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 
+array = ["volleyball", "basketball", "badminton"]
+single_string = "-"
+index = 0
+
+while index < array.length
+  array = array[index]
+  single_string += "#{array}-"
+  index = index + 1
+end
+
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
 
 # 10. Start with an array of numbers and compute the maximum number.
 #     For example, [5, 10, 8, 3] becomes 10.
+
+array = [5, 10, 8, 3]
+max_number = 0
+
+index = 0
+
+while index < array.length
+  array = array[index]
+  if max_number > array
+    max_number = array
+  end
+  index += 1
+end
+
+p max_number
 
 # #Alo Ladder Probs
 
