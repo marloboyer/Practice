90ff6b1346b327dae183ca0b9fea48db0a96e81b
marloboyer <marlo.boyer7@yahoo.com>
2024-01-07 17:45:12 -0600

    did 1o array while loop problems

--- more-practice.rb
+++ more-practice.rb
@@ -879,6 +879,177 @@ end
 
 p time_ten_plus_thirty(10)
 
+##Arrays Select Problems 
+
+#  1. Start with an array of numbers and create a new array with only the numbers less than 20.
+#     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
+
+numbers = [2, 32, 80, 18, 12, 3]
+under_20 = []
+index = 0 
+while index < number.length
+  number = numbers[index]
+  if number < 20 
+    under_20 << number
+  end 
+end 
+
+p under_20
+
+#  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+#     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
+
+strings = ["winner", "winner", "chicken", "dinner"] 
+starts_with_w = []
+index = 0 
+while index < strings.length 
+  string = strings[index]
+  if strings[0] == "w"
+    starts_with_w << strings[index]
+  end 
+  index += 1
+end 
+
+p starts_with_w
+
+
+#  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+expensive_items = []
+index = 0 
+while index < items.length
+  item = items[index]
+  if items[:price] > 5
+    expensive_items << item
+  end
+  index = index + 1
+end 
+
+p expensive_items
+
+#  4. Start with an array of numbers and create a new array with only the even numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+numbers = [2, 4, 5, 1, 8, 9, 7]
+even_numbers = []
+index = 0
+
+while index < numbers.length
+  number = numbers[index]
+  if numbers[index] % 2 == 0 
+    even_numbers << number
+  end
+  index = index + 1 
+end 
+
+p even_numbers
+
+#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+#     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+under_four = []
+index = 0
+
+while index < strings.length
+  string = strings[index]
+  if string.length < 4 
+    under_four << strings 
+  end 
+  index += index 
+end 
+
+p under_four
+
+#  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+under_six = []
+index = 0 
+
+while index < items.length
+  if items[index][:name].length < 6
+    under_six << items[index]
+  end 
+  index = index + 1
+end 
+
+p under_six
+
+#  7. Start with an array of numbers and create a new array with only the numbers less than 10.
+#     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
+
+numbers = [8, 23, 0, 44, 1980, 3]
+less_than_ten = []
+index = []
+
+while index < numbers.length 
+  number = numbers[index]
+  if numbers >= 23
+    less_than_ten << numbers[index]
+  end 
+  index = index + 1 
+end 
+
+p less_than_ten
+
+#  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+#     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
+
+strings = 
+no_b = []
+index = 0
+
+while index < strings.length
+  string = strings[index]
+  if strings[index][0] != b
+    no_b << strings[index]
+  end 
+  index = index + 1 
+end 
+
+p no_b
+
+
+#  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+under_ten = []
+index = []
+
+while index < items.length
+  item = items[index]
+  if items[:price] < 10
+    under_ten << items[index]
+  end 
+  index = index + 1 
+end 
+
+p under_ten 
+
+# 10. Start with an array of numbers and create a new array with only the odd numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
+
+numbers = 
+odd_numbers = []
+index = 0
+
+while index < numbers.length
+  number = numbers[index]
+  if numbers[index] %2 == 1
+    odd_numbers << numbers[index]
+  end 
+  index = index + 1
+end 
+
+p odd_numbers
+
+
+
 # #Alo Ladder Probs
 
 # # Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
