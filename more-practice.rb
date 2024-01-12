92e729556542f54fc3c5e6d723930a0b3f093d43
marloboyer <marlo.boyer7@yahoo.com>
2024-01-11 19:43:17 -0600

    did 5 practice problems of array reduce problems

--- more-practice.rb
+++ more-practice.rb
@@ -1048,6 +1048,107 @@ end
 
 p odd_numbers
 
+##Array Reduce Problems 
+
+#  1. Start with an array of numbers and compute the sum of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 26.
+
+numbers = [5, 10, 8, 3]
+sum = 0
+index = 0
+
+while index < numbers.length
+  sum = sum + numbers[index]
+  index = index + 1
+end 
+
+sum 
+
+#  2. Start with an array of strings and combine them all into a single string.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+
+sports = ["volleyball", "basketball", "badminton"]
+word = ""
+index = 0
+
+while index < sports.length
+  sports = sport[index]
+  word = word + sport
+  index = index + 1 
+end 
+ p word 
+
+#  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+sum = 0
+index = 0 
+
+while index < items.length
+sum = sum + items[index][:price]
+  index = index + 1
+end 
+
+p sum 
+
+
+#  4. Start with an array of numbers and compute the the minumum number.
+#     For example, [5, 10, 8, 3, 9] becomes 3.
+
+numbers = [5, 10, 8, 3, 9]
+minumum_number = 0
+index = 0 
+
+while index < numbers.length
+  nummbers = numbers[index]
+  if number < minumum_number
+    minumum_number = number
+  end 
+  index = index + 1 
+end 
+
+p minumum_number
+
+#  5. Start with an array of strings and compute the total length of all the strings.
+#     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+
+
+
+#  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+
+items = [{name: "chair", price: 100}, {name: "pencil", price: 1}
+lowest_price = 0 
+index = 0 
+
+while index < items.length 
+
+
+end 
+
+#  7. Start with an array of numbers and compute product of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 1200.
+
+numbers = [5, 10, 8, 3]
+product = 0
+index = 0 
+
+while index < numbers.length
+  product = product * numbers[index]
+  index = index + 1
+end 
+
+p product
+
+#  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+
+#  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+
+# 10. Start with an array of numbers and compute the maximum number.
+#     For example, [5, 10, 8, 3] becomes 10.
 
 
 # #Alo Ladder Probs
